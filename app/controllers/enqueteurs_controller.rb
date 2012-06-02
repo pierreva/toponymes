6b5b74efa3a61eed68f28c35074956df07848484
Pierre Vaudrey <pierre.vaudrey@gmail.com>
2012-06-02 18:16:56 +0200

    Implemented delete functionality for enqueteurs

--- app/controllers/enqueteurs_controller.rb
+++ app/controllers/enqueteurs_controller.rb
@@ -20,6 +20,25 @@ class EnqueteursController < ApplicationController
   def show
     @enqueteur = Enqueteur.find(params[:id])
   end
+  def edit
+  @enqueteur = Enqueteur.find(params[:id])
+  end
+  def update
+    @enqueteur = Enqueteur.find(params[:id])
+    if @enqueteur.update_attributes(params[:enqueteur])
+    flash[:notice] = "Enqueteur has been updated."
+    redirect_to @enqueteur
+    else
+    flash[:alert] = "Enqueteur has not been updated."
+    render :action => "edit"
+    end
+  end
+  def destroy
+  @enqueteur = Enqueteur.find(params[:id])
+  @enqueteur.destroy
+  flash[:notice] = "Enqueteur has been deleted."
+  redirect_to enqueteurs_path
+  end
 end
 
 
